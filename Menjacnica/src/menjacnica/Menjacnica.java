36b207547571b1d4e9e06c51e8c07729ee1677d4
Jelena-Milev <milev816@fon.bg.ac.rs>
2018-03-10 19:14:28 +0100

    Klasa Menjacnica izmenjena-implementira interfejs MenjacnicaInterfejs.
    Metode su sa praznim telom, menjacnica je nefunkcionalna.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,30 +1,30 @@
 package menjacnica;
 
+import java.util.GregorianCalendar;
 import java.util.LinkedList;
 
-public class Menjacnica {
+import menjacnica.interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs{
 
 	private LinkedList<Valuta> valute=new LinkedList<Valuta>();
 
-	public LinkedList<Valuta> getValute() {
-		return valute;
-	}
-
-	public void setValute(LinkedList<Valuta> valute) {
-		this.valute = valute;
+	@Override
+	public void dodajKurs(String valuta, GregorianCalendar datum, double prodajni, double srednji, double kupovni) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((valute == null) ? 0 : valute.hashCode());
-		return result;
+	public void obrisiKurs(String valuta, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
-	public String toString() {
-		return "Menjacnica > valute:" + valute;
+	public Kurs vratiKurs(String valuta, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	
